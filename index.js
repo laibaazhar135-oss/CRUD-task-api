8366567d4a30567e1a251cb302e7f30f2e503197
Laiba Azhar <laibaazhar135@gmail.com>
2026-07-21 18:04:23 +0500

    stage 2:read endpoints with 404

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let tasks=[
+  {id:1,title:"buy the milk",done:false},
+  {id:2,title:"walk the dog",done:false},
+  {id:3,title:"finish assignment",done:true}
+];
+
+app.get('/tasks',(req,res)=>{
+res.json(tasks);
+});
+
+app.get('/tasks/:id',(req,res)=>{
+const id=parseInt(req.params.id);
+const task=tasks.find(t=> t.id===id);
+if(!task){
+  return res.status(404).json({error:`Task ${id} not found`});
+}
+res.json(task);
+});
+
+
 app.get('/',(req,res)=>{
   res.json({
     name:"Task Api",
