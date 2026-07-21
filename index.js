a998583a99c4a29fde3ce252119ed8c53b43da8d
Laiba Azhar <laibaazhar135@gmail.com>
2026-07-21 16:14:26 +0500

    stage 1:roots and health endpoints

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/',(req,res)=>{
+  res.json({
+    name:"Task Api",
+    version:"1.0",
+    endpoints:["/tasks"]
+  })
+});
+
+app.get('/health',(req,res)=>{
+ res.json({
+  status:"ok"
+ })
 });
 
 app.listen(port, () => {
